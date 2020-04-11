bdde1c5e7126b55a488b8250f69865adf0e043e3
Lejick <lejick@gmail.com.com>
2020-04-11 09:10:00 +0300

    .

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a=-1;
+		assertEquals(Math.abs(a), abs(a));
+	}
+	public static final float abs(final float x) {
+			return StrictMath.abs(x);
 	}
 
 }
